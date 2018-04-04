918e86350ff32237f59aef5c0de759c07cf14ec5
ShikangJin <skjin@ucdavis.edu>
2018-04-03 18:02:20 -0700

    Add files via upload

--- Titan/src/rpc/SearchItem.java
+++ Titan/src/rpc/SearchItem.java
@@ -1,9 +1,9 @@
 package rpc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,14 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import db.DBConnection;
 import db.DBConnectionFactory;
 import entity.Item;
-import external.ExternalAPI;
-import external.ExternalAPIFactory;
 
 /**
  * Servlet implementation class SearchItem
@@ -27,7 +24,8 @@ import external.ExternalAPIFactory;
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private DBConnection conn = DBConnectionFactory.getDBConnection();
+    private DBConnection conn = DBConnectionFactory.getDBConnection();
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -43,13 +41,18 @@ public class SearchItem extends HttpServlet {
 		String userId = request.getParameter("user_id");
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
-		// Term can be empty or null.
+		//Term can be empty or null
 		String term = request.getParameter("term");
+		
 		List<Item> items = conn.searchItems(userId, lat, lon, term);
 		List<JSONObject> list = new ArrayList<>();
+		
+		Set<String> favorite = conn.getFavoriteItemIds(userId);
 		try {
 			for (Item item : items) {
+				// Add a thin version of item object
 				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favorite.contains(item.getItemID()));
 				list.add(obj);
 			}
 		} catch (Exception e) {
@@ -57,7 +60,7 @@ public class SearchItem extends HttpServlet {
 		}
 		JSONArray array = new JSONArray(list);
 		RpcHelper.writeJsonArray(response, array);
-	}
+ 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
